4a390b7f2c553e2067f6f2ca4ca540043bd6358d
SkillFactoryCSharpTest <skillfactory.csharp@mail.ru>
2021-10-19 18:03:15 +0300

    Добавьте файлы проекта.

---
+++ SkillFactory_Project56/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SkillFactory_Project56
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
